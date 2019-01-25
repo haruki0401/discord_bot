617a706262428b3021f69ad43b92b8af53e50111
haruki0401 <ynuharukisoccer@gmail.com>
2019-01-25 09:05:00 +0900

    some file deleted

--- key.py
+++
@@ -1,5 +0,0 @@
-# just state token
-# this file is pushed to heroku repo but is not to my repo.
-
-DISCORD_TOKEN = 'NTM2NzczODY3MTUxMTYzNDEy.DybmgQ.WDEH-EdsssAiWIfFg4-TRazHr1A'
-RIOT_TOKEN = 'RGAPI-4e90adc1-3cd2-4b44-b71b-6137d7afecb6'
