9f42994ca11de4141fbbeb3ba34d1a827f65b3a5
haruki0401 <ynuharukisoccer@gmail.com>
2019-01-25 20:00:12 +0900

    make it better

--- cogs/defaultcog.py
+++ cogs/defaultcog.py
@@ -1,13 +1,24 @@
 import discord
 from discord.ext import commands
 
-class Defaultcog:
+class Defaultcog(commands.Bot):
+
 
     def __init__(self, bot, riot):
         self.bot = bot
         self.riot = riot
 
-    # replay "にゃーん"
+
+
+
+    async def on_ready(self):
+        print("-----")
+        print('logged in')
+        print("-----")
+
+
+
+    # reply "にゃーん"
     @commands.command(pass_context=True)
     async def nyan(self,ctx):
         '''just say nyan~!!''' 
@@ -24,7 +35,7 @@ class Defaultcog:
                 embed = discord.Embed(description = "please enter 2 parameters!", color=0xeee657)
                 await self.bot.say(embed = embed)
             else:
-                if (type(args[0]) is int) and (type(args[1]) is int):
+                if args[0].isdigit() and args[1].isdigit():
                     ans = int(args[0]) + int(args[1])
                     embed = discord.Embed(description = ans, color=0xeee657)
                     await self.bot.say(embed = embed) 
@@ -35,44 +46,58 @@ class Defaultcog:
         
     # delete all msg (admin only)
     @commands.command(pass_context=True)
-    async def clean(self, ctx):
+    async def clean(self, ctx, *args):
         '''delete all msg (Only admin can usethis command)'''
         if ctx.message.channel.id == '536814382705934338'and ctx.message.author.server_permissions.administrator: # admin only command
-            clean_flag = True
-            while (clean_flag):
-                msgs = [msg async for msg in self.bot.logs_from(ctx.message.channel)]
-                if len(msgs) > 1: # 1発言以下でdelete_messagesするとエラーになる
-                    await self.bot.delete_messages(msgs)
-                else:
-                    clean_flag = False
-                    embed = discord.Embed(description = "deleted all msgs!!", color=0xeee657)
-                    await self.bot.say(embed = embed)
-
-    # get rank info
-    @commands.command(pass_context=True)
-    async def rank(self, ctx, *args):
-        '''get League of Legends rank info from SN (e.g. !rank Berry Summer)'''
-        if ctx.message.channel.id == '536814382705934338':
-            sn = ""
-            for str in args:
-                sn = sn + " " + str
             
-            sn = sn[1:]
-            print(sn)                
+            if(len(args) == 0):
+                #all delete
+                clean_flag = True
+                while (clean_flag):
+                    msgs = [msg async for msg in self.bot.logs_from(ctx.message.channel)]
+                    if len(msgs) > 1: # 1発言以下でdelete_messagesするとエラーになる
+                        await self.bot.delete_messages(msgs)
+                    else:
+                        clean_flag = False
+                        embed = discord.Embed(description = "deleted all msgs!!", color=0xeee657)
+                        await self.bot.say(embed = embed)
             
-            SID = self.riot.getID(sn)   
-            
-            if SID == "-1":
-                embed = discord.Embed(description = "The Summoner Name you entered does not exist.", color=0xeee657)
-                await self.bot.say(embed = embed)
-
+            elif(len(args) == 1 and args[0].isdigit()):
+                msgs = [msg async for msg in self.bot.logs_from(ctx.message.channel, limit = int(args[0]) + 1)]
+                await self.bot.delete_messages(msgs)
+                        
+    #get rank info
+    async def on_message(self, message):
+        if (message.content.startswith('/rank') or message.content.startswith('/r')) and message.channel.id == '536814382705934338':
+            query = message.content
+            if message.content.startswith('/rank'):
+                query = query[5:]
             else:
-                # first attribute : solo, second : flex
-                RANK = self.riot.getRank(SID)
-                if RANK == '-1':
-                    embed = discord.Embed(description = "Something error!", color=0xeee657)
-                    await self.bot.say(embed = embed)
+                query = query[2:]
+
+            if query[0] == " ":
+                query = query[1:]
+
+            #print(query)
+
+            SNs = query.split('\n')
+            #print(len(SNs))
+
+            for SN in SNs:
+                #print(SN)
+                SID = self.riot.getID(SN)   
+                
+                if SID == "-1":
+                    embed = discord.Embed(description = "SN : " + SN + " -> does not exist.", color=0xeee657)
+                    await self.bot.send_message(message.channel, embed = embed)
+
                 else:
-                    description = "Solo : " + RANK[0] + "\n" + "Flex : " + RANK[1]
-                    embed = discord.Embed(description = description, color=0xeee657)
-                    await self.bot.say(embed = embed)
+                    # first attribute : solo, second : flex
+                    RANK = self.riot.getRank(SID)
+                    if RANK == '-1':
+                        embed = discord.Embed(description = "Something error!", color=0xeee657)
+                        await self.bot.send_message(message.channel, embed = embed)
+                    else:
+                        description = "SN : " + SN + "\n" + "Solo : " + RANK[0] + "\n" + "Flex : " + RANK[1]
+                        embed = discord.Embed(description = description, color=0xeee657)
+                        await self.bot.send_message(message.channel, embed = embed)
