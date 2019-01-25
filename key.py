afe12e2cc0b12a1f1efd4a29b59c81cf7ba84bc3
haruki0401 <ynuharukisoccer@gmail.com>
2019-01-25 09:39:37 +0900

    deploy

---
+++ key.py
@@ -0,0 +1,5 @@
+# just state token
+# this file is pushed to heroku repo but is not to my repo.
+
+DISCORD_TOKEN = 'NTM2NzczODY3MTUxMTYzNDEy.DybmgQ.WDEH-EdsssAiWIfFg4-TRazHr1A'
+RIOT_TOKEN = 'RGAPI-4e90adc1-3cd2-4b44-b71b-6137d7afecb6'
